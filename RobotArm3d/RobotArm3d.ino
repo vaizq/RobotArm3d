c18293b3d753691eef583c03c589af8bab30a468
väinö heijari <vai.hei@hotmail.com>
2021-01-09 15:01:24 +0200

    Add project files.

---
+++ RobotArm3d/RobotArm3d.ino
@@ -0,0 +1,15 @@
+/*
+* Eliakses and Väinös arduino software for 3axis desktop robotic arm
+*/
+
+#include <Servo.h>
+
+Servo base;
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
