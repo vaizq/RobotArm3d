b396cc74ed15cb91220c361b597bd9906e38b9c1
väinö heijari <vai.hei@hotmail.com>
2021-01-10 16:42:44 +0200

    Rotary encoder inculded

--- RobotArm3d/RobotArm2d.h
+++ RobotArm3d/RobotArm2d.h
@@ -1,6 +1,6 @@
 #pragma once
+#include "Joystick.h"
 #include <Arduino.h>
-#include <Joystick.h>
 #include <Servo.h>
 
 class RobotArm2d
@@ -9,6 +9,8 @@ private:
 	Joystick myJoystick;
 	Servo servo0;
 	Servo servo1;
+
+
 public:
 	// Servos should be alrady attached
 	void attach(Joystick joystick, Servo servo1, Servo servo2);
--- RobotArm3d/RobotArm3d.ino
+++ RobotArm3d/RobotArm3d.ino
@@ -5,22 +5,28 @@
 #include "Point2d.h"
 #include "Joystick.h"
 #include "RobotArm2d.h"
+#include "RotaryEncoder.h"
 #include <Servo.h>
+
 
 // TODO: Elias pleas add stepper to this code.
 
-// Definitions
-#define SERVO_1 3
-#define SERVO_2 5
+// Definitions ( I changed servo pins! )
+#define SERVO_1 5
+#define SERVO_2 6
 #define JOYSTICK_VRX A0
 #define JOYSTICK_VRY A1
-#define JOYSTICK_SW 2
+#define JOYSTICK_SW -1
+#define ROTARYENCODER_CLK_PIN 2
+#define ROTARYENCODER_DT_PIN 3
+#define ROTARYENCODER_SW_PIN 4
 
 // Variables
 Servo servo1;
 Servo servo2;
 Joystick myJoystick;
 RobotArm2d robotArm;
+RotaryEncoder rEncoder;
 
 
 
@@ -31,13 +37,14 @@ void setup()
 	servo1.attach(SERVO_1);
 	servo2.attach(SERVO_2);
 	myJoystick.attach(JOYSTICK_VRX, JOYSTICK_VRY, JOYSTICK_SW);
+	rEncoder.attach(ROTARYENCODER_CLK_PIN, ROTARYENCODER_DT_PIN, ROTARYENCODER_SW_PIN);
 	robotArm.attach(myJoystick, servo1, servo2);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
-	robotArm.updatePos();
+	
 
-	delay(10);
+	delay(1);
 }
